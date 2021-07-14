51d16e384e69b69f4083bc722d3807b21c0c2422
jul <yulyarybalkina@yandex.ru>
2021-07-14 19:42:08 +0300

    +

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
+
+
 # uvicorn main:app --reload
 # http://127.0.0.1:8000
 # http://127.0.0.1:8000/docs
 # http://127.0.0.1:8000/redoc
+# http://127.0.0.1:8000/openapi.json
 
 
 @app.get("/")
