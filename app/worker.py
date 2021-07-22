6bc174b88e73cd5b45d1954b99bd5d1fe1c62d25
jul <yulyarybalkina@yandex.ru>
2021-07-22 16:08:32 +0300

    +

--- app/worker.py
+++ app/worker.py
@@ -1,43 +1,16 @@
 from celery import Celery
-import logging.config
 
 
 celery_app = Celery(__name__)
 celery_app.config_from_object('celeryconfig')
 
-celery_app.conf.task_routes = {
-    "app.celery.tasks.logging_task": "test-queue"
-}
+# celery_app.conf.task_routes = {
+#     "app.celery.tasks.logging_task": "test-queue"
+# }
+#
+# celery_app.conf.update(task_track_started=True)
 
-celery_app.conf.update(task_track_started=True)
 
-# настройка логирования:
-log_config = {
-    'version': 1,
-    'formatters': {
-        'basic': {
-            'format': '%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s',
-            'datefmt': '%d-%b-%y %H:%M:%S'
-        }
-    },
-    'handlers': {
-        'file_handler': {
-            'class': 'logging.FileHandler',
-            'formatter': 'basic',
-            'filename': 'app.log',
-            'mode': 'w',
-        },
-    },
-    'loggers': {
-        '': {
-            'level': 'NOTSET',
-            'handlers': ['file_handler']
-        }
-    }
-}
-
-logging.config.dictConfig(log_config)
-log = logging.getLogger(__name__)
 
 
 # @celery_app.task
--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@ import uvicorn
 
 # В одном терминале запускаем сервер Редис:
 # redis-server PycharmProjects/test_niisva/redis.conf
-# Во втором терминале (с виртуальной средой Python) запускаем celery:
-#
+# Во втором терминале (с виртуальной средой Python) запускаем celery (из корня, где "manage.py"):
+# celery -A worker app.worker.celery_app --loglevel=info
 # http://127.0.0.1:8080/docs
 
 if __name__ == '__main__':
